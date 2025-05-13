9b093bfa94e50fc0627941fcf42a11c2033251fc
Dany <dany-de-carvalho@outlook.fr>
2025-05-13 20:54:28 +0200

    api2

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true
   },
   webpack: (config) => {
     config.resolve.alias = {
