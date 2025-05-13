aa6382c07a5ec60305669c0f1fd714011a47e2ac
Dany <dany-de-carvalho@outlook.fr>
2025-05-13 22:13:01 +0200

    lets go

--- src/app/equipe/page.tsx
+++ src/app/equipe/page.tsx
@@ -4,6 +4,7 @@ import SelectionPokemon from "components/ecran/equipe/SelectionEquipe";
 import Pokedex from "components/Pokedex";
 
 export default async function EquipePage() {
+
     const res = await fetch("https://pokemonapi-production-b931.up.railway.app/pokemons/pokemons");
     const data = await res.json();
 
